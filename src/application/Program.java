aac323b33754214e421a0853f4ae01b1ddab19a6
BrunoFhome <brunodevjunior2022@gmail.com>
2026-01-15 17:08:20 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -47,6 +47,13 @@ public class Program {
 		System.out.println("Inserted! New ID: " + newSeller.getId());
 		
 		
+		System.out.println("\n=== TEST 5: seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
+		
+		
 	}
 
 }
