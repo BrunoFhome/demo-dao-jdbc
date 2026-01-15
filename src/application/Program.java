1a343e87fc342770fa74f2e74a526698a5140d9d
BrunoFhome <brunodevjunior2022@gmail.com>
2026-01-15 14:29:55 -0300

    FindById Implementatio

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,14 +13,10 @@ public class Program {
 	public static void main(String[] args) {
 		
 		
-		Department obj = new Department("Books", 1);
-		
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "Bruno", "bruno@gmail.com", new Date(), 3000.00, obj);
-		System.out.println(seller);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 
 }
